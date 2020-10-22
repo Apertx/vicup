9c7f6109b1ca972345e719c68ebab4eecb87f829
Apertx <Dirty0wolf@gmail.com>
2020-10-22 20:58:50 +0300

    js add

--- js/ooono.js
+++ js/ooono.js
@@ -1,6 +1,6 @@
 function showCall() {
   let div = document.createElement('div');
-  div.className = 'dial-float';
+  div.id = 'callHere';
   div.innerHTML = '<a href="tel:+79219397700"><div class="call-float"></div></a>';
   document.body.append(div);
 }
