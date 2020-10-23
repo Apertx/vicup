f2bb60882f0d3326191d1fae7bfc2e2bf7001d04
Apertx <Dirty0wolf@gmail.com>
2020-10-23 18:34:52 +0300

    float button anim

--- js/ooono.js
+++ js/ooono.js
@@ -1,6 +1,12 @@
 function showCall() {
-  let div = document.createElement('div');
-  div.id = 'callHere';
-  div.innerHTML = '<a href="tel:+79219397700"><div class="call-float"></div></a>';
-  document.body.append(div);
+  callHere.innerHTML = '<div class="email-float" data-toggle="modal" data-target="#modalMail"></div><a href="tel:+79219397700"><div class="call-float"></div></a><a href="javascript:hideCall();"><div class="clear-float"></div></a>';
+}
+
+function hideCall() {
+  callHere.innerHTML = '<div class="dial-circle"></div><div class="dial-pulse"></div><a href="javascript:showCall();"><div class="dial-float"></div></a>';
+}
+
+function closeCall() {
+  callHere.innerHTML = '<div class="dial-done"></div>';
+  setTimeout(()=>callHere.innerHTML='',1500);
 }
